a52e8577a540b9ad0bafc8ece438debe09940fd2
go <go@gmail.com>
2024-03-26 09:39:36 +0900

    revise02

--- src/Pages/Mobile/MobileTemaDetail.js
+++ src/Pages/Mobile/MobileTemaDetail.js
@@ -7,9 +7,15 @@ export default function MobileTemaDetail() {
       <div className={Style.temadetail}>
         <div className={Style.temadetail_visual}>
           <ul className={Style.temadetail_visual_list}>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>
+              <img src='../images/mobile_images/snowsnoopy_main01.jpg' alt='스노우스누피이미지01'/>
+            </li>
+            <li>
+            {/* <img src='../images/mobile_images/snowsnoopy_main02.jpg' alt='스노우스누피이미지02'/> */}
+            </li>
+            <li>
+            {/* <img src='../images/mobile_images/snowsnoopy_main03.jpg' alt='스노우스누피이미지03'/> */}
+            </li>
           </ul>
           <div className={Style.temadetail_txt}>
             <p className={Style.temadetail_category}>액티비티</p>
@@ -28,9 +34,12 @@ export default function MobileTemaDetail() {
             </div>
             <div className={Style.temadetail_intro}>
               <p className={Style.temadetail_intro_title}>행사정보</p>
-              <p className={Style.temadetail_intro_text}>
+              <p className={Style.temadetail_intro_txt}>
                 피너츠 친구들은 눈, 비, 바람, 낙엽과 같은 자연환경과 계절의 변화를 경험하면서 인생의 의미를 찾고 성장합니다. 겨울 분위기에서 만나는 피너츠 친구들과 함께 떠나보는 WINTER GARDEN
               </p>
+            </div>
+            <div className={Style.temadetail_price}>
+              성인 18,000원 / 청소년 15,000원 / 소인 13,000원 *36개월 미만 무료 입장
             </div>
           </div>
         </div>
