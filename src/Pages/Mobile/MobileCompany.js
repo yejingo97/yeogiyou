0bea3ef263488441302a15edcc006316d2ad6297
go <go@gmail.com>
2024-04-03 14:28:19 +0900

    revise0403 two

---
+++ src/Pages/Mobile/MobileCompany.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from './css/MobileCompany.module.css'
+
+export default function MobileCompany() {
+  return (
+    <div className={Style.company_wrap}>
+        MobileCompany
+    </div>
+  )
+}
